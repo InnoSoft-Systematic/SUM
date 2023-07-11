6d238511096d5d120d69d0d36de3d2c8fb7947c4
Matías <41658934+Ozarcus@users.noreply.github.com>
2023-07-11 12:01:39 -0300

    Menu funcional
    Primera version del menú principal

--- SUM.sh
+++ SUM.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
+#SUM v1.02 - Innosoft Systematic
 #
-#
+#Menú inicial para el llamado de los scripts
+echo "/**···¡Bienvenido a SUM!···**/"
+echo "1) Ingresar usuario"
+echo "2) Modificar usuario"
+echo "3) Eliminar usuario"
+echo "4) Gestionar grupos"
+echo "5) Acerca de"
+echo "0) Salir"
+read op
+
+function about () {
+	echo "/*****************SUM*****************/"
+	echo "Versión 1.02 para BASH"
+	echo "Gestor de usuarios y grupos de servidores Linux"
+	echo "InnoSoft Systematic - HellFire Team"
+	echo "Copyright 2023-2023 InnoSoft Systematic"
+	echo "/*****************SUM*****************/"
+	echo "Presiona cualquier tecla para salir"
+	read ap
+	case $ap in 
+		*)#clear (No funcional, para próxima entrega)
+			./SUM.sh
+	esac
+}
+
+case $op in
+	1)./cuser.sh;;
+	2)./muser.sh;;
+	3)./ruser.sh;;
+	4)./groups.sh;;
+	5)about;;
+	6)exit;;
+esac
+
+
