397456ca673463c13e1eaed454da26460eece34a
Matías <41658934+Ozarcus@users.noreply.github.com>
2023-07-11 12:04:58 -0300

    Se actualiza
    Se vuelve funcional

--- ruser.sh
+++ ruser.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 #
-#
+#SUM v1.02
+
+echo "1) Eliminar usuario"
+echo "2) Eliminar usuario y su directorio"
+echo "3) Volver"
+read op
+
+case $op in
+	1)echo "Ingresa nombre del usuario a eliminar"
+	read remove
+
+	if [ grep -q -i -w $remove /etc/passwd ]; then
+		userdel $remove
+		echo "El usuario $remove se ha eliminado exitosamente"
+	else 
+		echo "El usuario ingresado no existe"
+	fi	;;
+	2)echo "Ingresa nombre del usuario a eliminar"
+	#Sintaxis igual al de arriba, solo cambia la opción en el comando, el "-r" elimina el directorio personal del usuario
+	read remove1
+	if [ grep -q -i -w $remove1 /etc/passwd ]; then
+                userdel -r $remove1
+                echo "El usuario $remove1 y su directorio personal se ha eliminado exitosamente"
+        else
+                echo "El usuario ingresado no existe"
+        fi      ;;
+	3)./SUM.sh;;
+esac
