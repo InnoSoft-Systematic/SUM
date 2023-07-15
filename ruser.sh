edfe4b66858b886061e8c17af6b8f21022a722c2
Matías <41658934+Ozarcus@users.noreply.github.com>
2023-07-14 23:35:57 -0300

    Update ruser.sh
    Bug fixes

--- ruser.sh
+++ ruser.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-#SUM v1.02
+#SUM v1.0.2
+#Ver los parámteros, userdel -r es frágil
 
 echo "1) Eliminar usuario"
 echo "2) Eliminar usuario y su directorio"
@@ -11,7 +12,7 @@ case $op in
 	1)echo "Ingresa nombre del usuario a eliminar"
 	read remove
 
-	if [ grep -q -i -w $remove /etc/passwd ]; then
+	if grep -i -w $remove /etc/passwd; then
 		userdel $remove
 		echo "El usuario $remove se ha eliminado exitosamente"
 	else 
@@ -20,7 +21,7 @@ case $op in
 	2)echo "Ingresa nombre del usuario a eliminar"
 	#Sintaxis igual al de arriba, solo cambia la opción en el comando, el "-r" elimina el directorio personal del usuario
 	read remove1
-	if [ grep -q -i -w $remove1 /etc/passwd ]; then
+	if grep -i -w $remove1 /etc/passwd; then
                 userdel -r $remove1
                 echo "El usuario $remove1 y su directorio personal se ha eliminado exitosamente"
         else
