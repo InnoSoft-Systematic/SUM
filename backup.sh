005dadae2a1817d9605f25fc9ab6cc1c4c32099d
Matías <41658934+Ozarcus@users.noreply.github.com>
2023-10-08 18:48:31 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -10,10 +10,6 @@ tar -cf /etc/mysql/my.cnf | rsync -avh -e ssh --progress - user@hostdeservidorRE
 
 tar -cf /etc/ssh/sshd_config | rsync -avh -e ssh --progress - user@hostdeservidorRESPALDO:/home/backup
 
-tar -cf /var/log | rsync -avh -e ssh --progress - user@hostdeservidorRESPALDO:/home/backup
-
-tar -cf /var/log/syslog /var/log/auth.log | rsync -avh -e ssh --progress - user@hostdeservidorRESPALDO:/home/backup
-
 tar -cf /var/log/syslog /var/log/auth.log | rsync -avh -e ssh --progress - user@hostdeservidorRESPALDO:/home/backup
 
 tar -cf /var/lib/mysql | rsync -avh -e ssh --progress - user@hostdeservidorRESPALDO:/home/b
