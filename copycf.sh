24522d75025f88274599be55fa73d9d5a2bef3c2
Matías <41658934+Ozarcus@users.noreply.github.com>
2023-11-13 19:06:25 -0300

    Update copycf.sh
    Correción de errores

--- copycf.sh
+++ copycf.sh
@@ -4,7 +4,7 @@
 #
 tar -cf /etc/passwd /etc/shadow | rsync -avh -e ssh --progress - user@hostdeservidorRESPALDO:/home/backup
 
-tar -cf /etc/netwrok | rsync -avh -e ssh --progress - user@hostdeservidorRESPALDO:/home/backup
+tar -cf /etc/network | rsync -avh -e ssh --progress - user@hostdeservidorRESPALDO:/home/backup
 
 tar -cf /etc/ssh/sshd_config | rsync -avh -e ssh --progress - user@hostdeservidorRESPALDO:/home/backup
 
